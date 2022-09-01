e2c93f41296a31e8bd47b510abd04a1e98934a6d
elisejakob <hei@elisejakob.no>
2022-09-01 10:31:08 +0200

    update 404 page

--- web/src/pages/404.vue
+++ web/src/pages/404.vue
@@ -1,23 +1,50 @@
 <template>
   <Layout>
-    <PageHeader text="404" />
-    <section>
-      <h1>Oh no! This page is missing!</h1>
-      <p class="lead">For some reason we can't find the page you're looking for. Did you check the spelling in the URL? If you know for sure there was supposed to be a page here, please let us know. If not, try again from our front page :)</p>
-    </section>
-    <p>&rarr; <a href="/">Go home</a></p>
+    <PageHeader :content="pageHeader" />
+    <main class="page-content">
+      <p>
+        {{ message[$context.locale] }}
+      </p>
+      <p>
+        &rarr; <a href="/">{{ link[$context.locale] }}</a>
+      </p>
+    </main>
   </Layout>
 </template>
 
 <script>
-import PageHeader from '~/components/PageHeader'
+import PageHeader from "~/components/PageHeader";
 
 export default {
   components: {
-    PageHeader
+    PageHeader,
+  },
+  data() {
+    return {
+      pageHeader: {
+        heading: {
+          no: "404",
+          en: "404",
+        },
+        subheading: {
+          no: "Finner ikke siden",
+          en: "Missing page",
+        },
+      },
+      message: {
+        no:
+          "Av en eller annen grunn finner vi ikke siden du ser etter. Har du sjekket at du skrev URLen riktig? Hvis du er helt sikker på at det skulle vært en side her, gi oss gjerne beskjed. Hvis ikke, prøv igjen fra forsiden vår.",
+        en:
+          "For some reason we can't find the page you're looking for. Did you check the spelling in the URL? If you know for sure there was supposed to be a page here, please let us know. If not, try again from our front page.",
+      },
+      link: {
+        no: "Hjem",
+        en: "Home",
+      },
+    };
   },
   metaInfo: {
-    title: '404'
-  }
-}
+    title: "404",
+  },
+};
 </script>
