3b0468f2b607d336f069a179252476a22a90115a
elisejakob <hei@elisejakob.no>
2022-12-19 10:46:01 +0100

    start contact form

---
+++ web/src/components/ContactForm.vue
@@ -0,0 +1,58 @@
+<template>
+  <form name="reseller-contact" method="POST" data-netlify="true">
+    <label>Navn på bedrift: <input type="text" name="name"/></label>
+    <label>Kontaktperson navn: <input type="text" name="name"/></label>
+    <label>Kontaktperson telefon: <input type="phone" name="phone"/></label>
+    <label>Kontaktperson e-post: <input type="email" name="email"/></label>
+    <label>Melding: <textarea name="message"></textarea></label>
+    <button type="submit" class="button">Send</button>
+  </form>
+</template>
+
+<style lang="scss" scoped>
+label {
+  display: block;
+  margin-bottom: 1rem;
+  width: 30rem;
+  max-width: 100%;
+}
+input,
+textarea {
+  display: block;
+  width: 20rem;
+  max-width: 100%;
+  border: 1px solid var(--color-lightgray);
+  border-radius: var(--border-radius-l);
+  font-size: var(--font-size-s);
+  padding: 0.5rem 1rem;
+  margin: 0.25rem 0 0;
+  cursor: none;
+
+  &:hover {
+    border: 1px solid var(--cursor-color);
+    cursor: none;
+  }
+  &:focus {
+    outline: none;
+    border: 1px solid var(--cursor-color);
+    box-shadow: 0 0 0 1px var(--cursor-color);
+    cursor: none;
+  }
+}
+textarea {
+  width: 30rem;
+  height: 5rem;
+  border-radius: 1rem;
+}
+.button {
+  width: 12rem;
+  max-width: 100%;
+  background: var(--cursor-color);
+  border-color: var(--cursor-color);
+
+  &:hover {
+    background: var(--color-active);
+    border-color: var(--color-active);
+  }
+}
+</style>
