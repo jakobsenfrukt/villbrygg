fed2f4cb9b8facf9b4426a13df80d45e3dabf7b3
Jonas Braathen <jonas@ponas.no>
2022-12-19 20:52:46 +0100

    validate contact form, handle states, send a POST

--- web/src/components/ContactForm.vue
+++ web/src/components/ContactForm.vue
@@ -1,28 +1,131 @@
 <template>
-  <form name="reseller-contact" method="POST" data-netlify="true">
-    <label>
-      {{ $t("contactform.name") }}*
-      <input type="text" name="name" />
-    </label>
-    <label>
-      {{ $t("contactform.contactperson") }}
-      <input type="text" name="contactperson" />
-    </label>
-    <label>
-      {{ $t("contactform.phone") }}
-      <input type="tel" name="phone" />
-    </label>
-    <label>
-      {{ $t("contactform.email") }}*
-      <input type="email" name="email" />
-    </label>
-    <label>
-      {{ $t("contactform.message") }}
-      <textarea name="message"></textarea>
-    </label>
-    <button type="submit" class="button">{{ $t("contactform.submit") }}</button>
-  </form>
+  <div id="reseller-contact-wrapper">
+    <form
+      name="reseller-contact"
+      @submit="handleSubmit"
+      v-if="!submitted"
+      method="POST"
+      data-netlify="true"
+      netlify-honeypot="i-understand"
+      novalidate="true"
+    >
+      <input type="hidden" name="form-name" value="resellerContact" />
+      <label :class="markError('name')">
+        {{ $t("contactform.name") }}*
+        <input type="text" name="name" v-model="name" />
+        <span v-if="hasError('name')" class="error-message">{{
+          this.errors["name"]
+        }}</span>
+      </label>
+      <label>
+        {{ $t("contactform.contactPerson") }}
+        <input type="text" name="contactPerson" v-model="contactPerson" />
+      </label>
+      <label>
+        {{ $t("contactform.phone") }}
+        <input type="tel" name="phone" v-model="phone" />
+      </label>
+      <label :class="markError('email')">
+        {{ $t("contactform.email") }}*
+        <input type="email" name="email" v-model="email" />
+        <span v-if="hasError('email')" class="error-message">{{
+          this.errors["email"]
+        }}</span>
+      </label>
+      <label>
+        {{ $t("contactform.message") }}
+        <textarea name="message" v-model="message"></textarea>
+      </label>
+      <p class="hidden">
+        <label>
+          {{ $t("contactform.humansOnly") }}: <input name="i-understand" />
+        </label>
+      </p>
+      <button type="submit" class="button">
+        {{ $t("contactform.submit") }}
+      </button>
+    </form>
+    <div v-if="submitError">
+      <p>
+        {{ $t("contactform.errorTechnical") }}
+      </p>
+    </div>
+    <div v-if="submitted">
+      <p>{{ $t("contactform.greatSuccess") }}</p>
+      <button class="button" @click="reset()">
+        {{ $t("contactform.reset") }}
+      </button>
+    </div>
+  </div>
 </template>
+
+<script>
+const getInitialState = function () {
+  return {
+    submitted: false,
+    submitError: "",
+    errors: {},
+    name: "",
+    contactPerson: "",
+    phone: "",
+    email: "",
+    message: "",
+  };
+};
+
+const isEmail =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export default {
+  data() {
+    return getInitialState();
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.errors = {};
+
+      if (!this.name) {
+        this.errors["name"] = this.$t("contactform.errorRequiredField");
+      }
+
+      if (!this.email) {
+        this.errors["email"] = this.$t("contactform.errorRequiredField");
+      } else if (!isEmail.test(this.email)) {
+        this.errors["email"] = this.$t("contactform.errorEmailInvalid");
+      }
+
+      if (this.errors.length) {
+        return;
+      }
+
+      const formData = new FormData(e.target);
+      this.sendForm(formData);
+    },
+    sendForm(formData) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+      })
+        .then(() => (this.submitted = true))
+        .catch((error) => {
+          this.technicalError = error;
+          console.log(error);
+        });
+    },
+    markError(name) {
+      return { "has-error": this.hasError(name) };
+    },
+    hasError(name) {
+      return this.errors.hasOwnProperty(name);
+    },
+    reset() {
+      Object.assign(this.$data, getInitialState());
+    },
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 label {
@@ -61,6 +164,10 @@ textarea {
   width: 30rem;
   height: 7rem;
   border-radius: 1rem;
+}
+
+.hidden {
+  display: none;
 }
 .button {
   width: 12rem;
