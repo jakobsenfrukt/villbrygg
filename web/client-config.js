4570ac20a59e24d52323278ebe3ed1be6209032b
Jonas Braathen <jonas@ponas.no>
2024-07-12 16:48:05 +0200

    web: use test dataset

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
     projectId: process.env.GRIDSOME_SANITY_PROJECT_ID || '8pq05gnc',
-    dataset: process.env.GRIDSOME_SANITY_DATASET || 'production'
+    dataset: process.env.GRIDSOME_SANITY_DATASET || 'test'
   }
 }
