6d3037f801187f38a2a739d911734285aeeb45ce
Jonas Braathen <jonas@ponas.no>
2024-07-17 05:30:05 +0200

    customer-sync: log sanity project id and dataset

--- customer-sync/src/functions/sheets-to-sanity-background.js
+++ customer-sync/src/functions/sheets-to-sanity-background.js
@@ -2,6 +2,7 @@ import { syncAll } from "../index"
 
 export default async (req, context) => {
   console.log('start sheets-to-sanity-background')
+  console.log(`sanity projectId=${process.env.SANITY_PROJECT_ID} dataset=${process.env.SANITY_DATASET}`)
   await syncAll();
   console.log("done sheets-to-sanity-background");
 };
